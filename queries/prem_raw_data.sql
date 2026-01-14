345b8eadac82391d5d6b0f1513819fe65a48358a
ZacChisholm <zac4@st-andrews.ac.uk>
2026-01-14 13:40:38 +0000

    Some querying examples

---
+++ queries/prem_raw_data.sql
@@ -0,0 +1,175 @@
+-- Let's create a "raw data" table, i.e. what we would really expect a table with just the raw data to look like
+
+-- Create table to store imported data
+CREATE TABLE IF NOT EXISTS fixtures (
+    fixture_id INTEGER PRIMARY KEY,
+    fixture_time_raw VARCHAR,
+    country VARCHAR,
+    league VARCHAR,
+    season VARCHAR,
+    home_team VARCHAR,
+    away_team VARCHAR,
+    referee VARCHAR,
+    home_score INTEGER,
+    away_score INTEGER,
+    result VARCHAR
+);
+
+-- Import data
+COPY fixtures
+FROM '/Users/zacharychisholm/Downloads/prem_24_25_fixtures.csv'
+DELIMITER ',' CSV HEADER;
+
+-- Get rid of unnecessary columns
+ALTER TABLE fixtures
+    DROP COLUMN country,
+    DROP COLUMN league,
+    DROP COLUMN season,
+    DROP COLUMN result;
+
+-- Create the table storing all teams (what if a team changes its name, or its name is disputed?)
+CREATE TABLE IF NOT EXISTS teams (
+    team_id SMALLSERIAL PRIMARY KEY,
+    team_name VARCHAR NOT NULL
+);
+
+-- Populate teams table with team names
+INSERT INTO teams
+            (team_name)
+SELECT DISTINCT home_team
+FROM fixtures
+ORDER BY home_team;
+
+-- In fixtures table, create team ID columns (we want these instead of team names)
+ALTER TABLE fixtures
+    ADD COLUMN home_id INT,
+    ADD COLUMN away_id INT;
+
+-- Insert home team IDs
+UPDATE fixtures
+SET home_id = teams.team_id
+FROM teams
+WHERE fixtures.home_team = teams.team_name;
+
+-- Insert away team IDs
+UPDATE fixtures
+SET away_id = teams.team_id
+FROM teams
+WHERE fixtures.away_team = teams.team_name;
+
+-- Ensure the home and away team IDs are foreign keys; name these constraints
+ALTER TABLE fixtures
+    ADD CONSTRAINT fk_home_id
+        FOREIGN KEY (home_id) REFERENCES teams (team_id),
+    ADD CONSTRAINT fk_away_id
+        FOREIGN KEY (away_id) REFERENCES teams (team_id);
+
+-- Ensure there are two team IDs in every fixture; we don't need the team name columns anymore
+ALTER TABLE fixtures
+    ALTER COLUMN home_id SET NOT NULL,
+    ALTER COLUMN away_id SET NOT NULL,
+    DROP COLUMN home_team,
+    DROP COLUMN away_team;
+
+-- Actually, we don't like the abbreviated 'Utd' in the teams table
+UPDATE teams
+SET team_name = 'Manchester United'
+WHERE team_id = 14;
+
+-- Now we need to change the date formats from DMY to YMD
+-- Create a new column for fixture date/time in proper format
+ALTER TABLE fixtures
+    ADD COLUMN fixture_time TIMESTAMP;
+
+-- Populate new column with altered dates from original column
+UPDATE fixtures
+SET fixture_time = TO_TIMESTAMP(fixture_time_raw, 'DD-MM-YY HH24:MI');
+
+-- The times are actually an hour ahead
+UPDATE fixtures
+SET fixture_time = fixture_time - INTERVAL '1 hour';
+
+-- We can now drop the original column
+ALTER TABLE fixtures
+    DROP COLUMN fixture_time_raw;
+
+-- Add matchweek data
+-- Create table for matchweek data
+CREATE TABLE IF NOT EXISTS matchweek (
+    match_no INTEGER,
+    mw INTEGER,
+    date VARCHAR,
+    location VARCHAR,
+    home_team VARCHAR,
+    away_team VARCHAR,
+    result VARCHAR
+);
+
+-- Import matchweek data
+COPY matchweek
+FROM '/Users/zacharychisholm/Downloads/epl-2024-GMTStandardTime.csv'
+DELIMITER ',' CSV HEADER;
+
+-- Convert team names to team IDs (alphabetical order is in line with ID ordering)
+ALTER TABLE matchweek
+    ADD COLUMN home_id INTEGER,
+    ADD COLUMN away_id INTEGER;
+
+-- Create table to match matchweek team names to IDs
+CREATE TABLE IF NOT EXISTS mw_teams (
+    team_id SMALLSERIAL PRIMARY KEY,
+    mw_team VARCHAR
+);
+
+-- IDs go up in alphabetical order
+INSERT INTO mw_teams
+            (mw_team)
+SELECT DISTINCT home_team
+FROM matchweek
+ORDER BY home_team;
+
+-- Insert home team IDs
+UPDATE matchweek
+SET home_id = mw_teams.team_id
+FROM mw_teams
+WHERE matchweek.home_team = mw_teams.mw_team;
+
+-- Insert away team IDs
+UPDATE matchweek
+SET away_id = mw_teams.team_id
+FROM mw_teams
+WHERE matchweek.away_team = mw_teams.mw_team;
+
+-- We don't need the team names anymore
+ALTER TABLE matchweek
+    DROP COLUMN home_team,
+    DROP COLUMN away_team;
+
+-- Don't need the new ID table either
+DROP TABLE mw_teams;
+
+-- Finally, add in the matchweek for each fixture
+CREATE TABLE mw_fixtures AS
+    SELECT fixtures.*, matchweek.mw
+    FROM fixtures
+    LEFT JOIN matchweek
+        ON fixtures.home_id = matchweek.home_id
+        AND fixtures.away_id = matchweek.away_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ queries/prem_table_24_25.sql
@@ -0,0 +1,460 @@
+-- Premier League 2024-25 table
+SELECT
+    teams.team_name AS team,
+    COUNT(*) AS played,
+    SUM(wins) AS wins,
+    SUM(draws) AS draws,
+    SUM(losses) AS losses,
+    SUM(goals_for) AS goals_for,
+    SUM(goals_against) AS goals_against,
+    SUM(goals_for) - SUM(goals_against) AS goal_difference,
+    SUM(points) AS points,
+    ROUND(SUM(points)::DECIMAL / COUNT(*), 2) AS ppg
+FROM teams
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN home_score > away_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN home_score = away_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN home_score < away_score THEN 1 ELSE 0 END AS losses,
+        home_score AS goals_for,
+        away_score AS goals_against
+    FROM fixtures
+    WHERE fixture_time <= '2025-05-01 00:00:00'
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN home_score < away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN away_score > home_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN away_score = home_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN away_score < home_score THEN 1 ELSE 0 END AS losses,
+        away_score AS goals_for,
+        home_score AS goals_against
+    FROM fixtures
+    WHERE fixture_time <= '2025-05-01 00:00:00'
+) AS team_games ON teams.team_id = team_games.team_id
+GROUP BY teams.team_id
+ORDER BY points DESC, goal_difference DESC, goals_for DESC;
+
+-- Home table
+SELECT
+    teams.team_name AS team,
+    COUNT(*) AS played,
+    SUM(wins) AS wins,
+    SUM(draws) AS draws,
+    SUM(losses) AS losses,
+    SUM(goals_for) AS goals_for,
+    SUM(goals_against) AS goals_against,
+    SUM(goals_for) - SUM(goals_against) AS goal_difference,
+    SUM(points) AS points
+FROM teams
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN home_score > away_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN home_score = away_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN home_score < away_score THEN 1 ELSE 0 END AS losses,
+        home_score AS goals_for,
+        away_score AS goals_against
+    FROM fixtures
+    -- WHERE fixture_time <= '2024-12-25 00:00:00'
+) AS team_games ON teams.team_id = team_games.team_id
+GROUP BY teams.team_id
+ORDER BY points DESC, goal_difference DESC, goals_for DESC;
+
+-- Away table
+SELECT
+    teams.team_name AS team,
+    COUNT(*) AS played,
+    SUM(wins) AS wins,
+    SUM(draws) AS draws,
+    SUM(losses) AS losses,
+    SUM(goals_for) AS goals_for,
+    SUM(goals_against) AS goals_against,
+    SUM(goals_for) - SUM(goals_against) AS goal_difference,
+    SUM(points) AS points
+FROM teams
+INNER JOIN (
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN home_score < away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN away_score > home_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN away_score = home_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN away_score < home_score THEN 1 ELSE 0 END AS losses,
+        away_score AS goals_for,
+        home_score AS goals_against
+    FROM fixtures
+    -- WHERE fixture_time <= '2024-12-25 00:00:00'
+) AS team_games ON teams.team_id = team_games.team_id
+GROUP BY teams.team_id
+ORDER BY points DESC, goal_difference DESC, goals_for DESC;
+
+-- Goals by matchweek
+SELECT mw, SUM(home_score) + SUM(away_score) AS total_goals
+FROM mw_fixtures
+GROUP BY mw
+ORDER BY mw;
+
+-- Matches decided by 3+ goals
+SELECT f.*, t_home.team_name AS home_team, t_away.team_name AS away_team
+FROM fixtures f
+JOIN teams t_home
+    ON f.home_id = t_home.team_id
+JOIN teams t_away
+    ON f.away_id = t_away.team_id
+WHERE ABS(home_score - away_score) >= 3;
+
+-- Matches involving 3+ goals
+SELECT f.*, t_home.team_name AS home_team, t_away.team_name AS away_team
+FROM fixtures f
+JOIN teams t_home
+    ON f.home_id = t_home.team_id
+JOIN teams t_away
+    ON f.away_id = t_away.team_id
+WHERE home_score + away_score >= 3;
+
+-- Teams' goal difference per month
+SELECT
+    t.team_name AS team,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 8 THEN goal_difference ELSE 0 END) AS august,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 9 THEN goal_difference ELSE 0 END) AS september,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 10 THEN goal_difference ELSE 0 END) AS october,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 11 THEN goal_difference ELSE 0 END) AS november,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 12 THEN goal_difference ELSE 0 END) AS december,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 1 THEN goal_difference ELSE 0 END) AS january,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 2 THEN goal_difference ELSE 0 END) AS february,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 3 THEN goal_difference ELSE 0 END) AS march,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 4 THEN goal_difference ELSE 0 END) AS april,
+    SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 5 THEN goal_difference ELSE 0 END) AS may,
+    SUM(goal_difference) AS gd
+FROM teams t
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        home_score - away_score AS goal_difference,
+        fixture_time
+    FROM fixtures
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        away_score - home_score AS goal_difference,
+        fixture_time
+    FROM fixtures
+) f ON t.team_id = f.team_id
+GROUP BY t.team_name
+ORDER BY gd DESC;
+
+-- Teams' PPG per month
+SELECT
+    t.team_name AS team,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 8 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 8 THEN 1 ELSE 0 END), 2) AS august,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 9 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 9 THEN 1 ELSE 0 END), 2) AS september,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 10 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 10 THEN 1 ELSE 0 END), 2) AS october,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 11 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 11 THEN 1 ELSE 0 END), 2) AS november,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 12 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 12 THEN 1 ELSE 0 END), 2) AS december,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 1 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 1 THEN 1 ELSE 0 END), 2) AS january,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 2 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 2 THEN 1 ELSE 0 END), 2) AS february,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 3 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 3 THEN 1 ELSE 0 END), 2) AS march,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 4 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 4 THEN 1 ELSE 0 END), 2) AS april,
+    ROUND(SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 5 THEN points ELSE 0 END)::DECIMAL / SUM(CASE WHEN EXTRACT(MONTH FROM fixture_time) = 5 THEN 1 ELSE 0 END), 2) AS may,
+    ROUND(SUM(points)::DECIMAL / 38, 2) AS ppg
+FROM teams t
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        fixture_time
+    FROM fixtures
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN away_score > home_score THEN 3
+            WHEN away_score = home_score THEN 1
+            ELSE 0
+        END AS points,
+        fixture_time
+    FROM fixtures
+) f ON t.team_id = f.team_id
+GROUP BY t.team_name
+ORDER BY ppg DESC;
+
+-- London table
+SELECT
+    teams.team_name AS london_team,
+    COUNT(*) AS played,
+    SUM(wins) AS wins,
+    SUM(draws) AS draws,
+    SUM(losses) AS losses,
+    SUM(goals_for) AS goals_for,
+    SUM(goals_against) AS goals_against,
+    SUM(goals_for) - SUM(goals_against) AS goal_difference,
+    SUM(points) AS points
+FROM teams
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN home_score > away_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN home_score = away_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN home_score < away_score THEN 1 ELSE 0 END AS losses,
+        home_score AS goals_for,
+        away_score AS goals_against
+    FROM fixtures
+    WHERE home_id IN (1, 4, 6, 7, 9, 18, 19) AND away_id IN (1, 4, 6, 7, 9, 18, 19)
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN home_score < away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN away_score > home_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN away_score = home_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN away_score < home_score THEN 1 ELSE 0 END AS losses,
+        away_score AS goals_for,
+        home_score AS goals_against
+    FROM fixtures
+    WHERE home_id IN (1, 4, 6, 7, 9, 18, 19) AND away_id IN (1, 4, 6, 7, 9, 18, 19)
+) AS team_games ON teams.team_id = team_games.team_id
+GROUP BY teams.team_id
+ORDER BY points DESC, goal_difference DESC, goals_for DESC;
+
+-- Points per game before and after the halfway point of the season
+-- Table after matchweek 19 (but some games may have been postponed)
+SELECT
+    teams.team_name AS team,
+    COUNT(*) AS played,
+    SUM(wins) AS wins,
+    SUM(draws) AS draws,
+    SUM(losses) AS losses,
+    SUM(goals_for) AS goals_for,
+    SUM(goals_against) AS goals_against,
+    SUM(goals_for) - SUM(goals_against) AS goal_difference,
+    SUM(points) AS points
+FROM teams
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN home_score > away_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN home_score = away_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN home_score < away_score THEN 1 ELSE 0 END AS losses,
+        home_score AS goals_for,
+        away_score AS goals_against
+    FROM mw_fixtures
+    WHERE mw <= 19
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN home_score < away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN away_score > home_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN away_score = home_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN away_score < home_score THEN 1 ELSE 0 END AS losses,
+        away_score AS goals_for,
+        home_score AS goals_against
+    FROM mw_fixtures
+    WHERE mw <= 19
+) AS team_games ON teams.team_id = team_games.team_id
+GROUP BY teams.team_id
+ORDER BY points DESC, goal_difference DESC, goals_for DESC;
+
+-- What was the latest game in matchweek 19?
+SELECT fixture_time, mw
+FROM mw_fixtures
+WHERE mw = 19
+ORDER BY fixture_time DESC
+LIMIT 1;
+
+-- Had every team played 19 games once this game finished?
+SELECT
+    teams.team_name AS team,
+    COUNT(*) AS played,
+    SUM(wins) AS wins,
+    SUM(draws) AS draws,
+    SUM(losses) AS losses,
+    SUM(goals_for) AS goals_for,
+    SUM(goals_against) AS goals_against,
+    SUM(goals_for) - SUM(goals_against) AS goal_difference,
+    SUM(points) AS points
+FROM teams
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN home_score > away_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN home_score = away_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN home_score < away_score THEN 1 ELSE 0 END AS losses,
+        home_score AS goals_for,
+        away_score AS goals_against
+    FROM mw_fixtures
+    WHERE fixture_time <= '2025-01-02 00:00:00'
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN home_score < away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN away_score > home_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN away_score = home_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN away_score < home_score THEN 1 ELSE 0 END AS losses,
+        away_score AS goals_for,
+        home_score AS goals_against
+    FROM mw_fixtures
+    WHERE fixture_time <= '2025-01-02 00:00:00'
+) AS team_games ON teams.team_id = team_games.team_id
+GROUP BY teams.team_id
+ORDER BY points DESC, goal_difference DESC, goals_for DESC;
+
+-- Did Liverpool and Everton play immediately after?
+SELECT f.*, f.fixture_time,t_home.team_name AS home_team, t_away.team_name AS away_team
+FROM fixtures f
+JOIN teams t_home
+    ON f.home_id = t_home.team_id
+JOIN teams t_away
+    ON f.away_id = t_away.team_id
+WHERE home_id in (8, 12) AND away_id in (8, 12);
+-- No, so a fair halfway-point table isn't possible in terms of date
+
+-- PPG before and after end of matchweek 19 fixtures
+SELECT
+    teams.team_name AS team,
+    COUNT(*) AS played,
+    SUM(wins) AS wins,
+    SUM(draws) AS draws,
+    SUM(losses) AS losses,
+    SUM(goals_for) AS goals_for,
+    SUM(goals_against) AS goals_against,
+    SUM(goals_for) - SUM(goals_against) AS goal_difference,
+    SUM(points) AS points,
+    ROUND(SUM(CASE WHEN mw <= 19 THEN points ELSE 0 END)::DECIMAL / 19, 2) AS ppg1,
+    ROUND(SUM(CASE WHEN mw > 19 THEN points ELSE 0 END)::DECIMAL / 19, 2) AS ppg2,
+    ROUND(SUM(points)::DECIMAL / COUNT(*), 2) AS ppg
+FROM teams
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN home_score > away_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN home_score = away_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN home_score < away_score THEN 1 ELSE 0 END AS losses,
+        home_score AS goals_for,
+        away_score AS goals_against,
+        mw
+    FROM mw_fixtures
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN home_score < away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN away_score > home_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN away_score = home_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN away_score < home_score THEN 1 ELSE 0 END AS losses,
+        away_score AS goals_for,
+        home_score AS goals_against,
+        mw
+    FROM mw_fixtures
+) AS team_games ON teams.team_id = team_games.team_id
+GROUP BY teams.team_id
+ORDER BY points DESC, goal_difference DESC, goals_for DESC;
+
+-- Points by matchweek (for visualisation; see 24-25_race.R)
+SELECT
+    teams.team_name AS team,
+    SUM(points) OVER (PARTITION BY team_games.team_id ORDER BY mw) AS running_total
+FROM teams
+INNER JOIN (
+    SELECT
+        home_id AS team_id,
+        CASE
+            WHEN home_score > away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN home_score > away_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN home_score = away_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN home_score < away_score THEN 1 ELSE 0 END AS losses,
+        home_score AS goals_for,
+        away_score AS goals_against,
+        mw
+    FROM mw_fixtures
+
+    UNION ALL
+
+    SELECT
+        away_id AS team_id,
+        CASE
+            WHEN home_score < away_score THEN 3
+            WHEN home_score = away_score THEN 1
+            ELSE 0
+        END AS points,
+        CASE WHEN away_score > home_score THEN 1 ELSE 0 END AS wins,
+        CASE WHEN away_score = home_score THEN 1 ELSE 0 END AS draws,
+        CASE WHEN away_score < home_score THEN 1 ELSE 0 END AS losses,
+        away_score AS goals_for,
+        home_score AS goals_against,
+        mw
+    FROM mw_fixtures
+) AS team_games ON teams.team_id = team_games.team_id;
